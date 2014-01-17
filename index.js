fba019e200aedce95b21ed4f848698bfddd3c201
whiskers75 <whiskers75@me.com>
2014-01-17 14:49:22 +0000

    timers?

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Wolfgame module!
-var _k = Object.keys;
 var util = require('util');
 var Chance = require('chance');
+var _ = require('underscore');
 var chance = new Chance();
 var fs = require('fs');
 var coffee = require('coffee-script');
@@ -18,7 +18,7 @@ var Wolfgame = function() {
     this.checkEnd = function() {
 	var wolves = 0;
 	var vills = 0;
-	_k(process.game.players).forEach(function(player) {
+	_.keys(process.game.players).forEach(function(player) {
 	    if (process.game.players[player].team == 'wolf'){
 		wolves++;
 	    }
@@ -51,7 +51,7 @@ var Wolfgame = function() {
 		if (this.dead[player].onDeath) {
 		    player.onDeath(this, player.name);
 		}
-		_k(this.players).forEach(function(p) {
+		_.keys(this.players).forEach(function(p) {
 		    if (typeof p == 'undefined') {
 			return;
 		    }
@@ -73,13 +73,13 @@ var Wolfgame = function() {
     };
     this.autocomplete = function(player, from) {
 	var count = 0;
-        _k(this.players).forEach(function(p) {
+        _.keys(this.players).forEach(function(p) {
             if (p.indexOf(player) == 0) {
                 player = p;
 		count++;
             }
         });
-        if (_k(this.players).indexOf(player) == -1) {
+        if (_.keys(this.players).indexOf(player) == -1) {
 	    if (from) {
 		this.emit('notice', {to: from, message: 'That player does not exist.'});
 	    }
@@ -101,7 +101,7 @@ var Wolfgame = function() {
         if (this.phase !== 'day') {
             return;
         }
-        _k(this.lynches).forEach(function(lynch) {
+        _.keys(this.lynches).forEach(function(lynch) {
             lynch = process.game.lynches[lynch];
             if (!votes[lynch]) {
                 votes[lynch] = 0;
@@ -110,7 +110,7 @@ var Wolfgame = function() {
         });
         Object.keys(votes).forEach(function(vote) {
             var voted = votes[vote];
-            if (voted >= (_k(process.game.players).length - (_k(process.game.players).length > 4 ? 2 : 1))) {
+            if (voted >= (_.keys(process.game.players).length - (_.keys(process.game.players).length > 4 ? 2 : 1))) {
 		if (!process.game.kill(vote, ' was lynched by the angry mob of villagers.')) {
 		    process.game.emit('night');
 		}
@@ -128,18 +128,18 @@ var Wolfgame = function() {
 	}
     };
     this.randomPlayer = function() {
-        return _k(this.players)[chance.integer({min: 0, max: _k(this.players).length - 1})];
+        return _.keys(this.players)[chance.integer({min: 0, max: _.keys(this.players).length - 1})];
     };
     this.randomUPlayer = function() {
 	var roled = [];
-	_k(this.players).forEach(function(player) {
+	_.keys(this.players).forEach(function(player) {
 	    player = process.game.players[player];
 	    if (player != 'unallocated') {
 		roled.push(player);
 		delete process.game.players[player.name];
 	    }
 	});
-        var chosen = _k(this.players)[chance.integer({min: 0, max: _k(this.players).length - 1})];
+        var chosen = _.keys(this.players)[chance.integer({min: 0, max: _.keys(this.players).length - 1})];
 	roled.forEach(function(p) {
 	    process.game.players[p.name] = p;
 	});
@@ -169,7 +169,7 @@ var Wolfgame = function() {
 		    role.minPlayers = 0;
 		}
                 console.log('Loaded role: ' + role.toString() + ' (requires ' + role.minPlayers + ' players)');
-		if (_k(process.game.players).length >= role.minPlayers) {
+		if (_.keys(process.game.players).length >= role.minPlayers) {
 		    var torole = process.game.randomUPlayer();
 		    console.log('Allocating role: ' + role.toString() + ' to player ' + torole);
 		    process.game.players[torole] = role;
@@ -178,7 +178,7 @@ var Wolfgame = function() {
 		}
 	    });
             var defvil = new process.game.Villager(this);
-            _k(process.game.players).forEach(function(player) {
+            _.keys(process.game.players).forEach(function(player) {
                 if (process.game.players[player] == 'unallocated') {
                     process.game.players[player] = defvil;
                     process.game.players[player].name = player;
@@ -191,7 +191,7 @@ var Wolfgame = function() {
 	if (this.phase != 'start') {
 	    return this.emit('error', new Error('You can\'t join or quit now!'));
 	}
-	if (_k(this.players).indexOf(data.player) !== -1) {
+	if (_.keys(this.players).indexOf(data.player) !== -1) {
             this.emit('notice', {to: data.player, message: 'You are already playing.'});
 	}
 	else {
@@ -204,7 +204,7 @@ var Wolfgame = function() {
             return this.emit('error', new Error('You can\'t join or quit now!'));
         }
 	if (this.phase == 'start') {
-	    if (_k(this.players).indexOf(data.player) == -1) {
+	    if (_.keys(this.players).indexOf(data.player) == -1) {
 		return this.emit('notice', {to: data.player, message: 'You are not playing.'});
 	    }
 	    delete this.players[data.player];
@@ -225,11 +225,13 @@ var Wolfgame = function() {
 	this.timeouts.forEach(function(t) {
 	    clearTimeout(t);
 	});
+	setTimeout(function() {
 	this.timeouts.push(setTimeout(function() {
 	    if (this.phase == 'night') {
 		this.emit('day');
 	    }
 	}, 120000));
+	    }, 1000);
     });
     this.on('day', function() {
 	this.phase = 'day';
@@ -237,7 +239,7 @@ var Wolfgame = function() {
         this.timeouts.forEach(function(t) {
             clearTimeout(t);
         });
-	_k(this.players).forEach(function(p) {
+	_.keys(this.players).forEach(function(p) {
 	    if (typeof p == 'undefined') {
 		return;
 	    }
